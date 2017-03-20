9e88016b8effb6be0eaab5c377b56729fcf169db
t-rap <ths_raptor@web.de>
2017-03-20 16:58:48 +0100

    jdk von Java genommen, DexEnabled 1, DexOptions 2g

--- app/src/main/java/de/rasi/org/berlinvent/MainMenu.java
+++ app/src/main/java/de/rasi/org/berlinvent/MainMenu.java
@@ -1,67 +1,32 @@
 package de.rasi.org.berlinvent;
 
-import android.net.Uri;
+import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Xml;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.evdb.javaapi.APIConfiguration;
-import com.evdb.javaapi.EVDBAPIException;
-import com.evdb.javaapi.EVDBRuntimeException;
-import com.evdb.javaapi.data.Event;
-import com.evdb.javaapi.data.SearchResult;
-import com.evdb.javaapi.data.request.EventSearchRequest;
-import com.evdb.javaapi.operations.EventOperations;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.android.gms.maps.MapView;
 
-import org.simpleframework.xml.Serializer;
-import org.simpleframework.xml.core.Persister;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
-import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
-import retrofit2.http.GET;
 import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
 public class MainMenu extends AppCompatActivity implements View.OnClickListener {
 
     public String apikey = "QNgn6PCmNLN9HF9J";
-    public String base_url = APIConfiguration.getBaseURL();
+    //public String base_url = APIConfiguration.getBaseURL();
     public TextView t1;
     Button btn1, btn2, btn3;
-
+    View view = null;
 
     public HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
     public OkHttpClient client = new OkHttpClient.Builder()
@@ -69,14 +34,14 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener 
             .readTimeout(30, TimeUnit.SECONDS)
             .addInterceptor(logging)
             .build();
-    public  Retrofit retrofit = new Retrofit.Builder()
+    public Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("http://api.eventful.com/rest/")
             //.addConverterFactory(GsonConverterFactory.create(gson))
             .client(client)
             .addConverterFactory(SimpleXmlConverterFactory.create())
             .build();
     public EvtFulService service = retrofit.create(EvtFulService.class);
-    public Call<de.rasi.org.berlinvent.Eventlist> data =service.listLocation("Berlin","QNgn6PCmNLN9HF9J");
+    public Call<de.rasi.org.berlinvent.Eventlist> data = service.listLocation("Berlin", "QNgn6PCmNLN9HF9J");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,8 +50,7 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener 
         logging.setLevel(HttpLoggingInterceptor.Level.BODY);
 
 
-       // System.out.println(service.listLocation("QNgn6PCmNLN9HF9J","Berlin"));
-
+        // System.out.println(service.listLocation("QNgn6PCmNLN9HF9J","Berlin"));
 
 
         t1 = (TextView) findViewById(R.id.textmain);
@@ -111,17 +75,23 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener 
             //new GetUrlContentTask().execute("http://api.eventful.com/rest/events/search?app_key=QNgn6PCmNLN9HF9J&location=Berlin&callback");
         }
         if (v.getId() == R.id.button2)
-            t1.setText("Button 2 gedrückt");
+            sendMessage(setView());
+        t1.setText("Button 2 gedrückt");
         if (v.getId() == R.id.button3)
             t1.setText("Button 3 gedrückt");
     }
 
-    public void updateTextViewMain(String toThis) {
-        TextView textView = (TextView) findViewById(R.id.textmain);
-        textView.setText(toThis);
+    public void sendMessage(View view) {
+        Intent intent = new Intent(MainMenu.this, MapsActivity.class);
+        startActivity(intent);
     }
 
-    private class getListLocation extends AsyncTask<String, Integer, String>{
+    public View setView() {
+        view = (MapView) findViewById(R.id.map);
+        return view;
+    }
+
+    private class getListLocation extends AsyncTask<String, Integer, String> {
 
         @Override
         protected String doInBackground(String... params) {
@@ -138,7 +108,7 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener 
                 */
                 Eventlist all = test.body();
 
-
+/*
                 for(de.rasi.org.berlinvent.Event evt : all.getMatches()) {
                     System.out.println("Titel: " + evt.getTitle());
                     System.out.println("Beschreibung: " + evt.getDescription());
@@ -148,9 +118,9 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener 
                     System.out.println("URL: " + evt.getUrl());
                     System.out.println("----------");
                     content=content+"\n"+evt.getTitle()+evt.getDescription()+evt.getId()+evt.getStart_time()+evt.getStop_time()+evt.getUrl();
-                }
 
-
+            }
+*/
 
 
                 System.out.println("TEST101");
@@ -162,16 +132,16 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener 
             return content;
 
         }
+
         protected void onProgressUpdate(Integer... progress) {
         }
 
         protected void onPostExecute(String result) {
-            t1.setText("Result:"+result);
+            t1.setText("Result:" + result);
             //data.cancel();
 
         }
     }
-
 
 
 }
